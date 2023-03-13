498a5559cb590c56b299ccc79e0e2ed7c60caa71
ian <inreambonanza@gmail.com>
2023-03-13 23:00:21 +0800

    added machine random choice

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 function getComputerChoice(){
-    let rock = 1, paper = 2, scissors = 3;
 
-    let randomChoice = Math.floor(Math.random()*3 + 1);
+    const choices = ["rock","paper","scissors"]
 
-    return randomChoice;
+    let randomChoice = Math.floor(Math.random()*3);
+
+    //return randomChoice;
+
+    //Generate Random Computer Choice
+
+    if(randomChoice === 0){
+        console.log(`The computer chose ${choices[randomChoice]}`);
+    }
+    else if (randomChoice === 1){
+        console.log(`The computer chose ${choices[randomChoice]}`);
+    }
+    else{
+        console.log(`The computer chose ${choices[randomChoice]}`);
+    }
 
     console.log(randomChoice);
 
